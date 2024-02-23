d6a3ac65f4f82b748fc7fe41afb27997cc953eec
j-preim <j.preimesberger@gmail.com>
2024-02-23 12:57:36 -0600

    added app.jsx

---
+++ react-ecommerce/src/components/Products.jsx
@@ -0,0 +1,25 @@
+export default function Products(props) {
+  return (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {props.products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.qty}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
